5cdf01441591c1536d62a1fe63a75a8f100150cd
lekhweemeng <lek.hweemeng@live.com>
2020-04-29 22:47:19 +0800

    added script button to re-direct

--- public/script.js
+++ public/script.js
@@ -1,10 +1,25 @@
 console.log('hello!')
 
-registerButton = document.getElementById('register');
+registerButton = document.getElementById('register');tolanding
+intAddButton = document,getElementById('addanother');
+intLandingButton = document.getElementById('tolanding');
+
 
 const buttonClicked = function(){
     console.log('clicked!!');
     window.location.replace("/register")
 }
 
+const addClicked = function(){
+    console.log('clicked!!');
+    window.location.replace("/add")
+}
+
+const landingClicked = function(){
+    console.log('clicked!!');
+    window.location.replace("/landing")
+}
+
 registerButton.addEventListener('click',buttonClicked);
+intAddButton.addEventListener('click', addClicked);
+intLandingButton.addEventListener('click', landingClicked)
